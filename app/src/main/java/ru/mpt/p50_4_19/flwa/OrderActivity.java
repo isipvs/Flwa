901ddf13395ea4f1b84395786e5756b2936f4d8d
isipvs <isip_v.s.sulimov@mpt.ru>
2022-09-11 22:29:23 +0300

    initial

---
+++ app/src/main/java/ru/mpt/p50_4_19/flwa/OrderActivity.java
@@ -0,0 +1,14 @@
+package ru.mpt.p50_4_19.flwa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+    }
+}
